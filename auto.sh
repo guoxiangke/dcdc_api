147394df3cf10b46da6960283a2194a2a5b01a0e
xiangkeguo <xiangkeguo@linkdoc.com>
2018-06-03 23:19:41 +0800

    auto commit

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@
 pwd
 git pull && git commit -am 'auto commit' && git push
 cp -vvv ./html/composer.json ./drupal/composer.json
-cd ./drupal && git checkout drupal && git pull origin drupal && git commit -am 'auto commit composer.json' && git push origin drupal
+cd ./drupal && git checkout drupal && git pull origin drupal && git commit -am 'auto commit' && git push origin drupal
 ssh root@123.207.178.25<< EOF
 cd /root/html/dcdc_api/ && git checkout . && git pull && chmod +x ./run.sh && ./run.sh
 EOF
