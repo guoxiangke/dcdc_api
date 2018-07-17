809adf1357798479e82dfaf8d146441de47158e1
xiangkeguo <xiangkeguo@linkdoc.com>
2018-07-17 20:02:17 +0800

    auto commit

--- auto.sh
+++ auto.sh
@@ -2,7 +2,8 @@
 pwd
 git pull && git commit -am 'auto commit' && git push
 cp -vvv ./html/composer.json ./drupal/composer.json
-cd ./drupal && git checkout drupal && git pull origin drupal && git commit -am 'auto commit' && git push origin drupal
+cd ./drupal && git commit -am 'auto commit' && git push origin master
+cd ../ && git commit -am 'auto commit' && git push origin master
 ssh root@123.207.178.25<< EOF
-cd /root/html/dcdc_api/ && git checkout . && git pull && chmod +x ./run.sh && ./run.sh
+cd /root/html/dcdc_api/ && chmod +x ./run.sh && ./run.sh
 EOF
--- run.sh
+++ run.sh
@@ -1,41 +1,9 @@
 	#! /bin/bash
-	if [ -z "$1" ]
-	then
-	     ProjectName="ddApi"
-	else
-		ProjectName=$1
-	fi
-	if [ -z "$2" ]
-	then
-	     GitUrl="https://github.com/guoxiangke/dcdc_api.git"
-	else
-		GitUrl=$2
-	fi
-
-	if [ -z "$3" ]
-	then
-	    GitBranch="drupal"
-	else
-		GitBranch=$3
-	fi
-	echo Your ProjectName is : $ProjectName
-	echo your github url $GitUrl
-	echo your github Branch must be $GitBranch
-	drupalFolder="drupal"
-	mkdir -p ./$drupalFolder && cd ./$drupalFolder
-	if [ -d .git ]; then
-		git checkout .
-		git fetch
-		git checkout $GitBranch
-		git pull origin $GitBranch
-		cd ../
-		#todo if ./drupal/composer.yml update : docker build -t="drupal:composer" .
-	else
-		cd ../
-		rm ./$drupalFolder -rf
-		git clone -b $GitBranch $GitUrl ./$drupalFolder
-	fi;
-	
+	ProjectName="ddApi"
+	git checkout .
+	git pull origin master
+	git submodule foreach git checkout .
+	git submodule foreach git pull origin master
 	
 	docker-compose -p $ProjectName down --remove-orphans
 	docker-compose -p $ProjectName  up -d  --build --remove-orphans --force-recreate
