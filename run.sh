1e374710323b71a964a8d6b68ed2554378429140
Dale MacBook <xiangkeguo@gmail.com>
2018-11-29 16:39:33 +0800

    gg

--- run.sh
+++ run.sh
@@ -1,14 +1,22 @@
-	#! /bin/bash
-	ProjectName="ddApi"
-	git checkout .
-	git pull origin master
-	git submodule foreach git checkout .
-	git submodule foreach git pull origin master
-
-	docker-compose -p $ProjectName down --remove-orphans
-	docker-compose -p $ProjectName  up -d  --build --remove-orphans --force-recreate
-
-	docker restart a-nginx a-nginx-gen
-	docker-compose -p $ProjectName restart drupal
-	sleep 5
-	docker restart a-nginx
+#! /bin/bash
+# usage:
+# sh run.sh staging
+# sh run.sh production
+if [ -z "$1" ]
+then
+  stage="development"
+else
+  stage=$1
+fi
+echo $stage
+git checkout .
+git pull origin master
+git submodule foreach git checkout .
+git submodule foreach git pull origin master
+ProjectName="ddApi"
+docker-compose -f docker-compose-$stage.yml -p $ProjectName down --remove-orphans
+docker-compose -f docker-compose-$stage.yml -p $ProjectName  up -d  --build --remove-orphans --force-recreate
+docker restart a-nginx a-nginx-gen
+docker-compose -p $ProjectName restart drupal
+sleep 5
+docker restart a-nginx
